33d9f97a078ff4388e4fd175fb3569f53c37ce45
varsha13152 <varsha13152@gmail.com>
2025-02-12 00:37:35 +0800

    Revert "Update files to follow code quality and style standards"
    This reverts commit d8f664b152ed60a0329ab3cb65f8da92071bca05.

---
+++ src/main/java/exceptions/TabbyExceptionIncompleteCommand.java
@@ -0,0 +1,20 @@
+package exceptions;
+
+/**
+ * Exception thrown when a command is incomplete or missing required parts.
+ */
+public class TabbyExceptionIncompleteCommand extends TabbyException {
+    /**
+     * Constructs an {@code TabbyExceptionIncompleteCommand} with a detailed error message.
+     */
+    public TabbyExceptionIncompleteCommand() {
+        super("Invalid Command. Here are the valid commands & the respective formats:\n"
+                + "  - Mark: mark <task number>\n"
+                + "  - Unark: unmark <task number>\n"
+                + "  - Delete: delete <task number>\n"
+                + "  - Deadline: deadline <description> /by <date time>\n"
+                + "  - Event: event <description> /from <start> /to <end>\n"
+                + "  - Todo: todo <description>");
+    }
+}
+
--- src/main/java/exceptions/TabbyExceptionInvalidCommand.java
+++ src/main/java/exceptions/TabbyExceptionInvalidCommand.java
@@ -1,21 +1,18 @@
+/**
+ * This package contains custom exceptions used in the Tabby application to handle specific error cases.
+ */
 package exceptions;
 
 /**
- * Exception thrown when a command is incomplete or missing required parts.
+ * Exception thrown when the user input does not match any of the valid commands in the Tabby application.
  */
 public class TabbyExceptionInvalidCommand extends TabbyException {
+
     /**
-     * Constructs an {@code TabbyExceptionIncompleteCommand} with a detailed error message.
+     * Constructs a new TabbyExceptionInvalidCommand with a default error message.
+     * The error message specifies the list of valid commands available in the application.
      */
     public TabbyExceptionInvalidCommand() {
-        super("Invalid Command. Here are the valid commands & the respective formats:\n"
-                + "  - list: mark <task number>\n"
-                + "  - Mark: mark <task number>\n"
-                + "  - Unark: unmark <task number>\n"
-                + "  - Delete: delete <task number>\n"
-                + "  - Deadline: deadline <description> /by <date time>\n"
-                + "  - Event: event <description> /from <start> /to <end>\n"
-                + "  - Todo: todo <description>");
+        super("Invalid Command. Here are the valid commands: mark, unmark, delete, list, todo, event, deadline");
     }
 }
-
